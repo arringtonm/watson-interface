498717abd1b49c94ba272d1e48d5ae675206ce15
Michael Arrington <michaelarringtoniphone@gmail.com>
2018-01-30 16:07:07 -0800

    Add keyup event and timer

--- src/App.js
+++ src/App.js
@@ -3,47 +3,52 @@ $(document).ready(function() {
 
   console.log('ready');
 
-  $("#submit").submit(function(event) {
-    event.preventDefault();
-    translateLogic();
+  let timer = '';
+  let input = '';
 
+
+  $("#input").keyup(function() {
+    input = $("#input").val();
+    clearTimeout(timer);
+    timer = setTimeout(dummy, 500, input);
   });
-
 });
 
 
 
 
-translateLogic = function() {
-
-  alert("yo");
-
-
-  // hard coded test input
-
-
-
-  // let pageDomain = 'conversational';
-  // // hard coded language style. other options include 'news' and 'patent'
-  // let inputLang = 'en';
-  // // temporarily hard coded 'from' language.
-  // let outputLang = 'es'
-  //
-  // let model_id = inputLang + '-' + outputLang;
-  // let callData = {
-  //   model_id: model_id,
-  //   text: inputText
-  // };
+let dummy = function(input) {
+    $("#output").text('');
+    $("#output").text(input);
 }
 
-let restAPICall = {
-  type: 'POST',
-  url: '/api/translate',
-  // data: callData,
-  dataType: 'json',
-  headers: {
-    'X-WDC-PL-OPT-OUT': '0',
-    'X-Watson-Technology-Preview': '2017-07-01'
-  },
-  async: true
+
+let translator = function(input) {
+
+  // input as parameter
+
+  let pageDomain = 'conversational';
+  // hard coded language style. other options include 'news' and 'patent'
+  let inputLang = 'en';
+  // temporarily hard coded 'from' language.
+  let outputLang = 'es'
+
+  let model_id = inputLang + '-' + outputLang;
+  let callData = {
+    model_id: model_id,
+    text: inputText
+  };
+
+
+  let restAPICall = {
+    type: 'POST',
+    url: '/api/translate',
+    // data: callData,
+    dataType: 'json',
+    headers: {
+      'X-WDC-PL-OPT-OUT': '0',
+      'X-Watson-Technology-Preview': '2017-07-01'
+    },
+    async: true
+  }
 }
